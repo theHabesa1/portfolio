eaff93bdfc76ceed73373c18bc2f522143f67574
theHabesa1 <yaredabera215@gmail.com>
2022-09-30 18:50:48 +0300

    logo edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '250px',position:"relative", top:"30px" }} />
+        <img src={Logo} alt='Logo Image' style={{ width: '200px',position:"relative", top:"30px" }} />
       </div>
 
       {/* menu */}
